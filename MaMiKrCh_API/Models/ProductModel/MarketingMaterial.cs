198e0a6e9f1035fc314aef672b6fcf1edae4accc
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2019-11-19 11:22:13 +0100

    Model Moved to Api

---
+++ MaMiKrCh_API/Models/ProductModel/MarketingMaterial.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaMiKrCh.Models.ProductModel
+{
+	public abstract class MarketingMaterial
+	{
+		public Product Product { get; set; }
+		[Key]
+		public int Id { get; set; }
+
+		public abstract string ToHtml();
+	}
+}
