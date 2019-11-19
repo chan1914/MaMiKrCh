8c340cba9d7e9b6507cfa9db3b2804979fdda04d
Chanthosh Sivanandam <chan1914@edu.ucl.dk>
2019-11-19 11:11:43 +0100

    Ny API

--- MaMiKrCh/Startup.cs
+++ MaMiKrCh/Startup.cs
@@ -42,7 +42,7 @@ namespace MaMiKrCh
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env )
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger )
         {
             if (env.IsDevelopment())
             {
@@ -50,7 +50,8 @@ namespace MaMiKrCh
 			}
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+				
+				app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
