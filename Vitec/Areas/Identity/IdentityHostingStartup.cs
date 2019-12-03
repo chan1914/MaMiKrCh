0d9c99321fc34491422cccc540867d26e2f0d2ca
Chanthosh Sivanandam <chan1914@edu.ucl.dk>
2019-12-03 09:41:42 +0100

    hejd

---
+++ Vitec/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Vitec.Models;
+
+[assembly: HostingStartup(typeof(Vitec.Areas.Identity.IdentityHostingStartup))]
+namespace Vitec.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<VitecContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("VitecContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<VitecContext>();
+            });
+        }
+    }
+}
