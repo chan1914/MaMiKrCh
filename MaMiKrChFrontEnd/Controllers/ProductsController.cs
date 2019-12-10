10c223643ec542b18341a80cfbaded0f19cc0866
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2019-12-10 11:37:37 +0100

    Fixed ALL THE SHIT

---
+++ MaMiKrChFrontEnd/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaMiKrChFrontEnd.Models;
+using MaMiKrChFrontEnd.Models.API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaMiKrChFrontEnd.Controllers
+{
+    public class ProductsController : Controller
+    {
+		private ProductAPIHelper productAPIHelper = new ProductAPIHelper();
+		public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult ProductList()
+		{
+			ICollection<Product> products;
+			products = productAPIHelper.GetAllProducts().Result;
+			return View(products);
+		}
+	}
+}
---
+++ MaMiKrChFrontEnd/Models/API/ProductAPIHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MaMiKrChFrontEnd.Models.API
+{
+	public class ProductAPIHelper
+	{
+		public IConfiguration Configuration { get; set; }
+
+		public ProductAPIHelper()
+		{
+			Configuration = Startup.Configuration;
+		}
+
+		public async Task<ICollection<Product>> GetAllProducts()
+		{
+			string conStr = Configuration.GetConnectionString("ProductAPIConnection") + "api/Product/GetProducts";
+			List<Product> products;
+			using (HttpClient httpClient = new HttpClient())
+			{
+				using (var response = httpClient.GetAsync(
+					conStr))
+				{
+					string apiResponse = await response.Result.Content.ReadAsStringAsync();
+					products = JsonConvert.DeserializeObject<List<Product>>(apiResponse);
+				}
+			}
+			return products;
+		}
+	}
+}
