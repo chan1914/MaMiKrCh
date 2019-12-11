6eebad728bce9cd7b735a793d1251fac96744a3b
Maria Hovgaard Rasmussen <mari55fe@edu.eal.dk>
2019-12-11 12:13:54 +0100

    layout - ikke færdigt

--- MaMiKrChFrontEnd/Controllers/HomeController.cs
+++ MaMiKrChFrontEnd/Controllers/HomeController.cs
@@ -10,16 +10,32 @@ namespace MaMiKrChFrontEnd.Controllers
 {
 	public class HomeController : Controller
 	{
-		public IActionResult Index()
+		//public IActionResult Index()
+		//{
+		//	return View();
+		//}
+
+		public IActionResult Products()
 		{
 			return View();
 		}
 
-		public IActionResult Privacy()
+		public IActionResult About()
 		{
 			return View();
 		}
-
+		public IActionResult Contact()
+		{
+			return View();
+		}
+		public IActionResult Service()
+		{
+			return View();
+		}
+		public IActionResult Elements()
+		{
+			return View();
+		}
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
